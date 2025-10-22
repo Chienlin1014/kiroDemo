7f28e2649b6d72da492799579d568830128df301
Chienlin <shzerokimo@gmail.com>
2025-10-22 19:19:16 +0800

    Init

---
+++ src/main/java/com/course/kirodemo/KiroDemoApplication.java
@@ -0,0 +1,13 @@
+package com.course.kirodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiroDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiroDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course/kirodemo/KiroDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.kirodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiroDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
